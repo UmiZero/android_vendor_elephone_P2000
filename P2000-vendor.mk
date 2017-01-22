8aa95ad0313a35e6e17ab725dc8bbfb2df6b9312
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-01-22 16:29:00 +0100

    p2000: Bring up for Elephone P2000

---
+++ P2000-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/P2000/proprietary/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/P2000/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/P2000/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/P2000/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/P2000/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/elephone/P2000/proprietary/vendor,system/vendor)
